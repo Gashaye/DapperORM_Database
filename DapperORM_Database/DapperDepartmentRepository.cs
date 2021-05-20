459fa7596193fe8013e3244a979031a5d0b8c1cd
Gashaye <gashayem@gmail.com>
2021-05-19 21:08:42 -0400

    First Commit

---
+++ DapperORM_Database/DapperDepartmentRepository.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace DapperORM_Database
+{
+    class DapperDepartmentRepository : IDepartmentRepository
+    {
+        private readonly IDbConnection _connection;
+        //Constructor
+        public DapperDepartmentRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            return _connection.Query<Department>("SELECT * FROM Departments;");
+        }
+        public void InsertDepartment(string newDepartmentName)
+        {
+            _connection.Execute("INSERT INTO DEPARTMENTS (Name) VALUES (@departmentName);",
+                                new { departmentName = newDepartmentName });
+        }
+        public void UpdateDepartment(string updatedDepartmentName, int id)
+
+        {
+            _connection.Execute("update Departments set Name = 'ThinkPad laptop' where @DepartmentID = 8;", new { departmentName = updatedDepartmentName, DepartmentID = id});
+
+        }
+
+
+
+    }
+}
---
+++ DapperORM_Database/IDepartmentRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DapperORM_Database
+{
+    interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAllDepartments(); //Stubbed out method
+    }
+}
